6a483d8481bc9f38db2e4d3e02e718028689edaa
Keketso Matsuma <keketsomatsuma88@gmail.com>
2023-01-16 21:55:26 +0200

    Initial commit

---
+++ src/components/Theme.tsx
@@ -0,0 +1,53 @@
+import { createTheme } from "@mui/material/styles";
+import { green, } from "@mui/material/colors";
+export const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+  },
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          //   borderRadius: 20,
+          boxShadow: "none",
+          color: "black",
+          backgroundColor: "white",
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 20,
+          color: "white",
+          boxShadow: "none",
+          backgroundColor: green[700],
+          "&:hover": {
+            backgroundColor: green[500],
+            color: "white",
+          },
+        },
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          borderRadius: 20,
+          boxShadow: "3",
+        },
+      },
+    },
+  },
+});
---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import "./globalScss.scss";
+import "@fontsource/plus-jakarta-sans";
+import type { HeadFC, PageProps } from "gatsby";
+import { Box, Container } from "@mui/material";
+import Landing from "./Landing";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+
+import { theme } from "../components/Theme";
+const IndexPage: React.FC<PageProps> = () => {
+  return (
+    <Box
+      flex={1}
+      sx={{
+        flex: 1,
+        height: "100vh",
+        overflowY: "auto",
+        // background: "black",
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        <Landing></Landing>
+      </ThemeProvider>
+    </Box>
+  );
+};
+
+export default IndexPage;
